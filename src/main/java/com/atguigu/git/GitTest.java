a7caec8dcc16f4c5afa2d9493c5a7fb391f33ac3
xiao <1986524434@qq.com>
2022-06-28 17:08:22 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,10 +2,10 @@ package com.atguigu.git;
 
 /**
  * @author xiao
- * @create 2022-06-28 17:02
  */
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello git");
         System.out.println("hello git");
     }
 }
