27ca163834561334e35be4fe17dabd0b5d624582
xiao <1986524434@qq.com>
2022-06-28 17:48:47 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git hot");
         System.out.println("hello git master-test");
         System.out.println("hello git hot-fix");
+        System.out.println("hello git push test");
     }
 }
